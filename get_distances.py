20ce4b594f75fd460c00fd79885679ef0deaa588
itamar <itamarreis@mail.tau.ac.il>
2019-09-09 23:21:07 +0300

    get distances as run

--- get_distances.py
+++ get_distances.py
@@ -6,11 +6,11 @@ sys.path.append('unsupervised-random-forest/')
 from unsupervised_random_forest import urf
 
 
-specra_path   = '/storage/home/itamarreis/git/manga/spectra.npy'
+specra_path   = '/storage/home/itamarreis/git/manga/test_data/spectra_final.npy'
 spectra = numpy.load(specra_path)
 #spectra = numpy.random.rand(1000,100)
 
-spectra = pp.norm_spectra(spectra)
+#spectra = pp.norm_spectra(spectra)
 
 urf_ = urf(n_trees = 100)
 distance_matrix = urf_.get_distance(spectra)
